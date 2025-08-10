a737f1d680923c8e12315c861fd54abbe96e4101
Md.Samiul Haque <samiulh4@gmail.com>
2025-08-11 00:40:26 +0600

    anchor taag error solved

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const anchorElement = {
     type : 'a',
     title: 'Click me to visit google !',
     props:{
-        href:'htpps://goolge.com',
+        href:'https://www.google.com/',
         target:'_blank'
     }
 }
@@ -11,6 +11,8 @@ function renderElement(element, container)
 {
     const domElement = document.createElement(element.type)
     domElement.innerHTML = element.title
+    domElement.setAttribute('href', element.props.href)
+    domElement.setAttribute('target', element.props.target)
 
     container.appendChild(domElement)
 }
